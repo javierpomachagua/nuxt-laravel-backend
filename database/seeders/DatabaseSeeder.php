695e59c5ad5e0673b678c6a105a1b8cf6ce8ddf7
Javier Pomachagua <javierpp0303@gmail.com>
2021-02-21 11:32:44 -0500

    [Add] Laravel Sanctum API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::forceCreate([
+            'email' => 'admin@admin.com',
+            'name' => 'admin',
+            'password' => bcrypt('password')
+        ]);
     }
 }
